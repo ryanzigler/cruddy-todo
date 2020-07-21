64006394f173626ea39f70af88784789650e8e92
Mike Shertz <shertzm2@gmail.com>
2020-07-21 17:37:52 -0400

    Implemented delete and now passing all 18 tests successfully

--- datastore/index.js
+++ datastore/index.js
@@ -9,20 +9,18 @@ var items = {};
 
 exports.create = (text, callback) => {
   counter.getNextUniqueId( (err, id) => {
-    // var fileName =
     fs.writeFile((exports.dataDir + '/' + id + '.txt'), text, (err) => {
       if (err) {
         throw ('error saving Todo to disk');
       } else {
-        callback(null, {id, text})
+        callback(null, {id, text});
       }
-    })
-  })
+    });
+  });
 };
 
 
 exports.readAll = (callback) => {
-
   fs.readdir(exports.dataDir, (err, files) => {
     if (err) {
       throw ('error retrieving all items');
@@ -31,9 +29,8 @@ exports.readAll = (callback) => {
         _.map(files, (file) => {
           var id = path.basename(file, '.txt');
           return ({id: id, text: id});
-        })
+        });
       )
-      console.log("All files read succesfully");
     }
   });
 };
@@ -45,9 +42,9 @@ exports.readOne = (id, callback) => {
     if (err) {
       callback(new Error(`No item at ID: ${id}`));
     } else {
-      callback(null, {id: id, text: data})
+      callback(null, {id: id, text: data});
     }
-  })
+  });
 };
 
 exports.update = (id, text, callback) => {
@@ -58,24 +55,24 @@ exports.update = (id, text, callback) => {
     } else {
       fs.writeFile(filePath, text, (err) => {
         if (err) {
-          callback(new Error(`No item at ID: ${id}`))
+          callback(new Error(`No item at ID: ${id}`));
         } else {
           callback(null, {id, text});
         }
-      })
+      });
     }
   });
 };
 
 exports.delete = (id, callback) => {
-  var item = items[id];
-  delete items[id];
-  if (!item) {
-    // report an error if item not found
-    callback(new Error(`No item with id: ${id}`));
-  } else {
-    callback();
-  }
+  var filePath = exports.dataDir + '/' + id + '.txt';
+  fs.unlink(filePath, (err) => {
+    if (err) {
+      callback(new Error(`No item at ID: ${id}`));
+    } else {
+      callback(null);
+    }
+  });
 };
 
 // Config+Initialization code -- DO NOT MODIFY /////////////////////////////////
